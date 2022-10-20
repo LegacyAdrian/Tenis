500cc08476777c6f144508b9848b7b377b46cfb5
Adrian <legacyadrian2.0@gmail.com>
2022-10-20 19:39:49 +0100

    COnfiguración del Entorno

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  plugins: ['jest'],
+};
